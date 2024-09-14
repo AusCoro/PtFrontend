516239ac6c61eee330b29dd70765cd3d0f0ef70a
Carlos Y. Ruiz T. <cyair99@gmail.com>
2024-09-13 19:07:17 -0500

    Fix responsive bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    RouterModule
-  ],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   logout() {
     this.authService.logout();
@@ -20,10 +18,27 @@ export class HomeComponent {
   }
 
   toggleSidebar() {
-    console.log('toggleSidebar');
-    const sidebar = document.getElementById('homee');
+    const sidebar = document.getElementById('logo-sidebar');
     if (sidebar) {
-      sidebar.classList.toggle('hidden');
+      sidebar.classList.toggle('-translate-x-full');
+    }
+  }
+
+  // Cerrar sidebar si se hace clic fuera del mismo
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    const sidebar = document.getElementById('logo-sidebar');
+    const toggleButton = document.querySelector(
+      '[aria-controls="logo-sidebar"]'
+    );
+
+    if (sidebar && toggleButton && window.innerWidth < 640) {
+      if (
+        !sidebar.contains(event.target as Node) &&
+        !toggleButton.contains(event.target as Node)
+      ) {
+        sidebar.classList.add('-translate-x-full');
+      }
     }
   }
 }

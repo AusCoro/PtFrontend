6d2c0d18667d4fe679b43302c09c3530e4b857b1
Carlos Y. Ruiz T. <cyair99@gmail.com>
2024-09-19 22:52:28 -0500

    Add modal and button

--- src/app/screens/reports/reports.component.ts
+++ src/app/screens/reports/reports.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SharedModule } from "../../shared/shared.module";
 import { ReportsInterface } from '../../models/report';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, CommonModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
 
 export class ReportsComponent {
+  showModal: boolean = false;
   reports: ReportsInterface[] = [
     {
-      id: '11231231',
+      id: '11',
       numRef: 1,
       numBDO: 233223,
       create_date: '05/05/2023',
@@ -22,10 +24,10 @@ export class ReportsComponent {
       station: 'A1',
       airline: 'Aeroméxico',
       deliveryZone: 'Hotel',
-      status: 'En preogreso'
+      status: 'En progreso'
     },
     {
-      id: '2212312',
+      id: '22',
       numRef: 2,
       numBDO: 233223,
       create_date: '05/05/2023',
@@ -37,115 +39,7 @@ export class ReportsComponent {
       status: 'Finalizado'
     },
     {
-      id: '2212312',
-      numRef: 3,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '',
-      destination: 'Yucatan',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'Esperando'
-    },
-    {
-      id: '11231231',
-      numRef: 1,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '',
-      destination: 'Cancun',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'En preogreso'
-    },
-    {
-      id: '2212312',
-      numRef: 2,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '05/05/2023',
-      destination: 'Consumel',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'Finalizado'
-    },
-    {
-      id: '2212312',
-      numRef: 3,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '',
-      destination: 'Yucatan',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'Esperando'
-    },
-    {
-      id: '11231231',
-      numRef: 1,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '',
-      destination: 'Cancun',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'En preogreso'
-    },
-    {
-      id: '2212312',
-      numRef: 2,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '05/05/2023',
-      destination: 'Consumel',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'Finalizado'
-    },
-    {
-      id: '2212312',
-      numRef: 3,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '',
-      destination: 'Yucatan',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'Esperando'
-    },
-    {
-      id: '11231231',
-      numRef: 1,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '',
-      destination: 'Cancun',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'En preogreso'
-    },
-    {
-      id: '2212312',
-      numRef: 2,
-      numBDO: 233223,
-      create_date: '05/05/2023',
-      finish_date: '05/05/2023',
-      destination: 'Consumel',
-      station: 'A1',
-      airline: 'Aeroméxico',
-      deliveryZone: 'Hotel',
-      status: 'Finalizado'
-    },
-    {
-      id: '2212312',
+      id: '33',
       numRef: 3,
       numBDO: 233223,
       create_date: '05/05/2023',
@@ -159,22 +53,25 @@ export class ReportsComponent {
   ];
 
   tableHeaders = ['Num.Ref', 'Num.BDO', 'Fecha de creacion', 'Fecha de finalizacion' ,'Destino', 'Estación', 'Aerolínea', 'Zona de entrega', 'Estatus'];
+
+
   
   tableData: any[] = [];
   ngOnInit(): void {
-    this.tableData = this.reports.map(report => [
-      report.id,
-      report.numRef,
-      report.numBDO,
-      report.create_date,
-      report.finish_date ? report.finish_date : 'N/A',
-      report.destination,
-      report.station,
-      report.airline,
-      report.deliveryZone,
-      report.status
-    ]);
-
+    this.tableData = this.reports.map(report => ({
+      data: [
+        report.numRef,
+        report.numBDO,
+        report.create_date,
+        report.finish_date ? report.finish_date : 'N/A',
+        report.destination,
+        report.station,
+        report.airline,
+        report.deliveryZone,
+        report.status
+      ],
+      id: report.id
+    }));
   }
 
   
--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
   @Input() data: any[] = [];
@@ -13,6 +13,8 @@ export class TableComponent implements OnInit {
   filters: string[] = [];
   showFilters: boolean[] = [];
   showStatusModal: boolean = false;
+  selectedRowId: string | null = null;
+  selectedStatus: string = '';
 
   onFilterChange(event: Event, index: number): void {
     const input = event.target as HTMLInputElement;
@@ -21,18 +23,34 @@ export class TableComponent implements OnInit {
   }
 
   onStatusClick(rowIndex: number) {
+    this.selectedRowId = this.filteredData[rowIndex].id;
     this.showStatusModal = true;
   }
 
   applyFilters(): void {
-    this.filteredData = this.data.filter(row => {
+    this.filteredData = this.data.filter((row) => {
       return this.filters.every((filter, index) => {
         if (!filter) {
           return true;
         }
-        return row[index].toString().toLowerCase().includes(filter);
+        return row.data[index].toString().toLowerCase().includes(filter);
       });
     });
+  }
+
+  // Nueva función para obtener el id seleccionado
+  getSelectedRowId(): string | null {
+    return this.selectedRowId;
+  }
+
+  // Función para manejar la actualización del estado
+  onUpdateStatus(): void {
+    const id = this.getSelectedRowId();
+    if (id) {
+      console.log(`ID seleccionado: ${id}`);
+      console.log(`Estado seleccionado: ${this.selectedStatus}`);
+    }
+    this.showStatusModal = false;
   }
 
   ngOnInit(): void {
